38232fb6b4293cad908496de838584bfa0fcd109
Roshan Khatri <rvkhatri@amazon.com>
2025-03-26 01:57:17 +0000

    intial commit
    Signed-off-by: Roshan Khatri <rvkhatri@amazon.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+# valkey-extension
+
+This Project is the Git repo of the [Valkey Extension "Official Image"](https://hub.docker.com/r/valkey/valkey-extension/)
+
+The Project is maintained by [the Valkey Community](https://github.com/valkey-io/)
+
+## When should a new image built and publish new Docker Image?
+
+A new Docker Image should built and published after a new major, minor or patch version of Valkey or any Valkey module releases.
+
+## How do you build and publish new version of a Docker Image?
+
+*Pre-requisites: [Fork](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo) this repo, create a private Docker Hub repo, and setup your GitHub secrets to access the private Docker Hub repo.*
+
+1. Bump up the respective versions in versions.json. (will be automated by implementing versions.sh)
+2. Run `apply-templates.sh`
+3. Update the `dockerhub-description.md` with the updated tags.
+4. Open a new PR for the new changes.
+5. Once the PR is merged, Sit back, relax and enjoy looking at your creation getting published to the official Valkey-extension Docker Hub page.
---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+set -Eeuox pipefail
+
+declare -A aliases=(
+	[8.1]='8 latest'
+)
+
+self="$(basename "$BASH_SOURCE")"
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+if [ "$#" -eq 0 ]; then
+	versions="$(jq -r 'keys | map(@sh) | join(" ")' versions.json)"
+	eval "set -- $versions"
+fi
+
+# sort version numbers with highest first
+IFS=$'\n'; set -- $(sort -rV <<<"$*"); unset IFS
+
+# get the most recent commit which modified any of "$@"
+fileCommit() {
+	git log -1 --format='format:%H' HEAD -- "$@"
+}
+
+# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
+dirCommit() {
+    local dir="$1"; shift
+    (
+        cd "$dir"
+        fileCommit \
+            Dockerfile \
+            $(git show HEAD:./Dockerfile | awk '
+                toupper($1) == "COPY" {
+                    # Skip the COPY keyword
+                    for (i = 2; i < NF; i++) {
+                        # Skip --from=build and similar flags
+                        if ($i !~ /^--from=/) {
+                            # If the path doesnt start with /, assume its a local path
+                            if ($i !~ /^\//) {
+                                print $i
+                            }
+                        }
+                    }
+                }
+            ')
+    )
+}
+
+cat <<-EOH
+# this file is generated via https://github.com/valkey-io/valkey-extension/blob/$(fileCommit "$self")/$self
+
+Maintainers: Roshan Khatri <rvkhatri@amazon.com> (@roshkhatri)
+GitRepo: https://github.com/valkey-io/valkey-extension.git
+EOH
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
+
+for version; do
+	export version
+
+	fullVersion="$(jq -r '.[env.version].version' versions.json)"
+
+	versionAliases=()
+	while [ "$fullVersion" != "$version" ] && [ "${fullVersion%.*}" != "$fullVersion" ]; do
+		versionAliases+=( $fullVersion )
+		fullVersion="${fullVersion%.*}"
+	done
+	versionAliases+=(
+		$version
+		${aliases[$version]:-}
+	)
+
+	for variant in debian; do
+		export variant
+		dir="$version/$variant"
+
+		commit="$(dirCommit "$dir")"
+
+		if [ "$variant" = 'debian' ]; then
+			variantAliases=( "${versionAliases[@]}" )
+		else
+			variantAliases=( "${versionAliases[@]/%/-$variant}" )
+			variantAliases=( "${variantAliases[@]//latest-/}" )
+		fi
+
+		if [ "$variant" = 'debian' ]; then
+			suite="bookworm"
+            suiteAliases=( "${versionAliases[@]/%/-$suite}" )
+            suiteAliases=( "${suiteAliases[@]//latest-/}" )
+		    variantAliases+=( "${suiteAliases[@]}" )
+        fi
+
+
+		echo
+		cat <<-EOE
+			Tags: $(join ', ' "${variantAliases[@]}")
+			GitCommit: $commit
+			Directory: $dir
+		EOE
+	done
+done
